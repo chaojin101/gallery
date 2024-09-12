d3760e5b44b320ed51e98b98b5d56d1a4365cc47
jam <chaojin101@gmail.com>
2024-09-11 20:26:03 -0700

    make client access the variable

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -6,3 +6,11 @@ const Page = () => {
 };
 
 export default Page;
+
+const setupEnv = () => {
+  if (!process.env.NEXT_PUBLIC_BACKEND_HOST) {
+    throw Error(
+      "Please set the `BACKEND_HOST` variable in the `.env` file to connect to the backend service. Refer to the `.env.example` file for guidance."
+    );
+  }
+};
--- apps/web/src/backend.ts
+++ apps/web/src/backend.ts
@@ -1,11 +1,6 @@
+"use client";
+
 import { treaty } from "@elysiajs/eden";
 import { type App } from "@gallery/backend";
 
-export const backend = treaty<App>("localhost:3000");
-
-async function signUp() {
-  const response = await backend.api.v1.users["sign-up"].post({
-    email: "string",
-    password: "string",
-  });
-}
+export const backend = treaty<App>(process.env.NEXT_PUBLIC_BACKEND_HOST);
